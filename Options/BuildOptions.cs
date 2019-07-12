26ba6f9887f73e79c87cc97eafdec6e0bbf31d9e
MegaJohnny <megajohnny@hotmail.co.uk>
2019-07-12 22:16:22 +0100

    Fix loose option being on by default

--- Options/BuildOptions.cs
+++ Options/BuildOptions.cs
@@ -5,7 +5,7 @@ namespace XModPackager.Options
     [Verb("build", HelpText = "Write mod contents to output directory.")]
     public class BuildOptions
     {
-        [Option("loose", Required = false, Default = true, HelpText = "If true, build the mod as loose files rather than a zip file.")]
+        [Option("loose", Required = false, Default = false, HelpText = "If true, build the mod as loose files rather than a zip file.")]
         public bool Loose {get; set;}
     }
 }
